755b7d6a39fda6a8a811dd967684badc87919277
xuesongzuo@yunify.com <xuesongzuo@yunify.com>
2025-04-12 16:50:48 +0800

    change docekr file

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+BUILDPATH=./${1:?"path to build"}
 BUILD_GOOS=${GOOS:-$(go env GOOS)}
 BUILD_GOARCH=${GOARCH:-$(go env GOARCH)}
 GOBINARY=${GOBINARY:-go}
@@ -7,4 +8,4 @@ ${GOBINARY} mod vendor
 # forgoing -i (incremental build) because it will be deprecated by tool chain.
 GOOS=${BUILD_GOOS} CGO_ENABLED=0 GOARCH=${BUILD_GOARCH} ${GOBINARY} build \
         -o /usr/local/bin/server \
-        http3.go
+        "${BUILDPATH}"
